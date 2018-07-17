328d824854bc2bc71f648a7696f5ddde52e09514
Jon Catmull <jonathan.catmull@room58.com>
2018-07-17 10:06:51 +0100

    header bug fixed

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
 import { DealershipService } from '../services/dealership.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -15,10 +16,12 @@ export class HeaderComponent implements OnInit {
     constructor(public authService: AuthService, public dealershipService: DealershipService) {
         // this.dealershipSelect = new FormControl('',Validators.required);
         const disabledState = (authService.user) ? Boolean(authService.user.websites.length < 2) : false;
+        console.log('this.dealershipService.dealershipId',this.dealershipService.dealershipId);
         this.dealershipSelect = new FormControl({value:this.dealershipService.dealershipId, disabled: disabledState, validators: [Validators.required]});
     }
 
     ngOnInit() {
+        this.dealershipService.dealershipId$.pipe(take(1)).subscribe(id => this.dealershipSelect.setValue(id));
         this.dealershipSelect.valueChanges.subscribe(val => {
             this.dealershipService.dealershipId = parseInt(val);
         });
